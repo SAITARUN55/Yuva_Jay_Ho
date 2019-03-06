dae580d7ef2753371ba232466c7f0c1e682c0445
Kaniganti Sai Tarun <SAITARUN55>
2019-03-06 08:57:33 +0530

    Added New Layouts About MP and edited Existing Layouts

---
+++ app/src/main/java/com/saar/saitarun/yuvajayho/AboutMP.java
@@ -0,0 +1,13 @@
+package com.saar.saitarun.yuvajayho;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AboutMP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_mp);
+    }
+}
--- app/src/main/java/com/saar/saitarun/yuvajayho/MainActivity.java
+++ app/src/main/java/com/saar/saitarun/yuvajayho/MainActivity.java
@@ -8,14 +8,13 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        submit = (Button)findViewById(R.id.submit);
+        Button submit = (Button)findViewById(R.id.submit);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
